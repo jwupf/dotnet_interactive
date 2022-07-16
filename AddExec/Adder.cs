9171c7d67737c6590a390b6738fcfa56606d27f2
Jörg Wunderlich-Pfeiffer <jwupf@gmx.net>
2022-07-16 17:42:20 +0200

    Initial

---
+++ AddExec/Adder.cs
@@ -0,0 +1,12 @@
+﻿namespace adder;
+public class Adder
+{
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+}
